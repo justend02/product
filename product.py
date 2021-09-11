e29cd814f3899ad5e649d803398ec41ea8d71df8
Jerry Wang <kuanchieh.w@gmail.com>
2021-09-11 10:32:03 +0900

    add header

--- product.py
+++ product.py
@@ -4,8 +4,16 @@ while True:
 	if name == 'q':
 		break
 	price = input('please enter the price: ') 
+	num = int(price)
 	products.append([name, price])
 print(products)
 
 for p in products:
-	print(p[0], 's price is', p[1])
+	print(p[0], "'s' price is", p[1])
+
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('product, price\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) +'\n') #有逗號會分成兩格
+
+
