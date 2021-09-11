ea4ac65ae4f9a8b04a0663985dd4d0e3ef1a7b64
Jerry Wang <kuanchieh.w@gmail.com>
2021-09-12 07:39:45 +0900

    first version

--- product.py
+++ product.py
@@ -1,19 +1,8 @@
 products = []
 while True:
-	name = input('product name: ')
+	name = input('please enter product name: ')
 	if name == 'q':
 		break
 	price = input('please enter the price: ') 
-	num = int(price)
 	products.append([name, price])
 print(products)
-
-for p in products:
-	print(p[0], "'s' price is", p[1])
-
-with open('products.csv', 'w', encoding='utf-8') as f:
-	f.write('product, price\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) +'\n') #有逗號會分成兩格
-
-
